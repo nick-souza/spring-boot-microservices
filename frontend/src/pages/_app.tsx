f2b8c4255e906bfa9c82c85f9bd640b47ec8948f
nick-souza <nsds26@hotmail.com>
2023-10-20 11:35:09 -0300

    fixed the removal of holderStyle

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -14,12 +14,6 @@ export default function App({ Component, pageProps }: any) {
 	const [mounted, setMounted] = useState(false);
 	useEffect(() => setMounted(true), []);
 
-	if (typeof window !== "undefined") {
-		window.onload = () => {
-			document.getElementById("holderStyle")!.remove();
-		};
-	}
-
 	Router.events.on("routeChangeStart", () => {
 		document.body.style.overflow = "hidden";
 		setLoading(true);
@@ -34,16 +28,18 @@ export default function App({ Component, pageProps }: any) {
 		<>
 			{/* Used to prevent flash from unstyled content on antd version 5 */}
 			<ConfigProvider>
-				<style
-					id="holderStyle"
-					dangerouslySetInnerHTML={{
-						__html: `
-							*, *::before, *::after {
-								transition: none!important;
-							}
-                    	`,
-					}}
-				/>
+				{!mounted && (
+					<style
+						id="holderStyle"
+						dangerouslySetInnerHTML={{
+							__html: `
+								*, *::before, *::after {
+									transition: none!important;
+								}
+							`,
+						}}
+					/>
+				)}
 				<div style={{ visibility: !mounted ? "hidden" : "visible" }}>
 					<Head>
 						<title>Agendamentos</title>
